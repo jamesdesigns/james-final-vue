432482966fc8998e9be83e688ebd4f2d4ecf53cd
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-07 23:24:46 -0700

    Add Login file

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+<v-container>
+    <v-layout justify-center>
+        <v-flex xs6>
+            <v-card>
+                <v-form>
+                    <v-text-field
+                    label="Email"
+                    v-model="email"
+                    placeholder="foo@bar.com">
+                    </v-text-field>
+                    <v-text-field
+                    label="Password"
+                    v-model="password"
+                    :type="show1 ? 'text' : 'password'">
+                    </v-text-field>
+                </v-form>
+            </v-card>    
+        <v-flex> 
+            <div>
+                <p>{{ email }}</p>
+                <p>{{ password }}</p>
+            </div>       
+    </v-layout>    
+</v-container>    
+</template>
+<script>
+</script>
+<style>
+</style>
