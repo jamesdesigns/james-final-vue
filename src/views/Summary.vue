027718ac0f8f95c93fdf922755fb4d9ac500ad2c
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-08 14:10:08 -0700

    Add Summary page

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -1,12 +1,18 @@
 <template>
+<div>
 <h1>Hello World!</h1>
 <p>This is a sentence.</p>
 <p>This is a sentence.</p>
 <p>This is a sentence.</p>
 <p>This is a sentence.</p>
 <p>This is a sentence.</p>
+</div>
 </template>
 <script>
 </script>
-<style>
+<style scoped>
+h1 {
+    font-size: 300%;
+}
+
 </style>
