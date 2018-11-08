6c68eb7fabd449dd7f0a0f2f7bdcaaa8ef89b20b
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-07 23:33:23 -0700

    Erase Login template

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 
+
 Vue.use(Router);
 
 export default new Router({
@@ -19,6 +20,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
 });
