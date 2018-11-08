da82c3d3c46b68606dd64f03573253ba9bdd2868
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-08 13:14:29 -0700

    Make revisions

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,38 +1,10 @@
 <template>
   <div class="about">
- <v-container grid-list-xl>
-   <v-layout row wrap>
-     <v-flex v-for="profile in profiles" :key="profile.name" xs4>
-       <v-card
-       height="225px">
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">{{ profile.surname }}</h3>
-            <div>{{ profile.age }}</div>
-            <span>{{ profile.gender }}
-          </div>  
-        </v-card-title>
-
-        <v-card-action>
-          <v-btn flat color="blue">Search</v-btn>
-        </v-card-action> 
-           
-       </v-card>
-     </v-flex>  
-  </v-layout>  
- </v-container>  
+ 
   </div>
 </template>
 <script>
-import { profiles } from '../assets/profiles'
 
-export default {
-  data: function() {
-    return {
-      allProfiles: profiles
-    };
-  }
-};
 </script>
 <style>
 </style>
