02e0559a43f5540e6f8604ed9651b71e6802a71d
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-08 11:50:55 -0700

    Add color to toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
+    <v-app> 
+     <v-toolbar color="cyan" dark app clipped-left><v-toolbar-side-icon @click.stop="drawer = !drawer">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+     <router-link to="/">Home</router-link> &nbsp;&nbsp;
+      <router-link to="/about">About</router-link> 
     </div>
+    </v-toolbar-side-icon>
+    </v-toolbar>
     <router-view/>
+    </v-app>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -18,14 +32,18 @@
 }
 #nav {
   padding: 30px;
+  margin-left: 3%;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+  padding-left: 175px;
+  text-decoration: none;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>sdlfkjsldkfjsldkfjsdlfkj</p>
+    <p>sdlfkjsldkfjsldkfjsdlfkj</p>
+    <p>sdlfkjsldkfjsldkfjsdlfkj</p>
+    <p>sdlfkjsldkfjsldkfjsdlfkj</p>
   </div>
 </template>
+<script>
+</script>
+<style>
+</style>
