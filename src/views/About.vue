1affd5249531c1b4a1c4f4845c09624d3a4374eb
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-14 11:55:18 -0700

    Add clarity to how the app is supposed to work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>How it Works</h1>
-    <p>Create a team the easy way. Search for individuals who may be interested on the Members page. Simply select who you would like to join your new team. You can choose who might benefit you the most. Once you have selected your ideal team, click Send Request and each hand picked member will either approve or disapprove your request.</p>
+    <p>Create a team the easy way. Search for individuals who may be interested in teaming up with you. Simply select who you would like to join your new team. Choose individuals who might benefit you the most. Once you have selected your ideal team, click APPROVE and each hand picked member will either approve or disapprove your request.</p>
 
   </div>
 </template>
