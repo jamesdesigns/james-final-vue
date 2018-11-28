8c982e18f8c38490c6a6be0241f06afe5828ed43
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-28 12:58:46 -0700

    Hide the svg in the Home component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,16 @@
 <template>
 <div>
-
-<!--
- <v-tabs
-    color="purple"
-    dark
-    slider-color="purple lighten-3"
-  >
-    <v-tab
-      v-for="i in 3"
-      :key="i"
-      ripple
-    >
-      Item {{ i }}
-    
-  </v-tabs>
-
-
-
- -->   
-
-
-
     <v-app> 
        <v-toolbar color="black" dark app clipped-left>
-           <v-toolbar-title class="font-weight-black"><router-link class="menu" to="/"><img src="../assets/logo-icon.png" alt="TeamUp" />TeamUp</router-link></v-toolbar-title>
+           <v-toolbar-title class="font-weight-black"><img src="../assets/logo-icon.png" alt="TeamUp" />TeamUp</v-toolbar-title>
         <div id="nav">
         <v-tabs color="black" dark slider-color="pink lighten-1">
-        <v-tab ripple><router-link class="menu" to="/">Home</router-link></v-tab>
-        <v-tab ripple><router-link class="menu" to="/members">Members</router-link></v-tab>
-        <v-tab ripple><router-link class="menu" to="/about">About</router-link></v-tab>
-        <v-tab ripple><router-link class="menu" to="/login">Login</router-link></v-tab>
-
-         
+            <v-tab ripple><router-link class="menu" to="/">Home</router-link></v-tab>
+            <v-tab ripple><router-link class="menu" to="/members">Members</router-link></v-tab>
+            <v-tab ripple><router-link class="menu" to="/about">About</router-link></v-tab>
+            <v-tab ripple><router-link class="menu" to="/login">Login</router-link></v-tab>
         </v-tabs>
       </div>
-      
         </v-toolbar>
         <router-view/>
     </v-app>
