af7e24dd8919de38811510278834829b0ea2e95d
James Hooper <jameshooperdesigns@yahoo.com>
2018-11-29 17:09:40 -0700

    Add custom directive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <main-header></main-header>
-   <!-- <p v-highlight:background.delayed="'gray'">TeamUp</p> -->
+    <p v-highlight:background.delayed="'gray'">TeamUp</p>
   </div>
 </template>
 <script>
