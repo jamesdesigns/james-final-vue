39ad19041732fd4fa74192012420e4b38b4fcec6
James Hooper <jameshooperdesigns@yahoo.com>
2018-12-03 23:07:34 -0700

    Adding three rows of member profiles in the Member component

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -2,11 +2,82 @@
 <div>
 <!--  <div class="container"> -->
     <v-container fluid>
-    <v-layout column>
-   <v-flex xs12 sm6 offset-sm3 v-for="i in 20" :key="i" xs4>
+    <v-layout 
+    row
+    align-center
+    justify-center
+    ma-0
+    >
+   <v-flex v-for="i in 4" :key="i">
   <div>
       <v-card width="200px">
        <v-img :src="`https://randomuser.me/api/portraits/women/${i + 20}.jpg`" alt="image" width="200px" height="200px">
+        
+        </v-img>
+        <v-card-title>
+            <ul>
+                <li class="headline mb-0">{{ info.data.name }}</li>
+                <li>Age: {{ info.data.age }}</li>
+                <li>Gender: {{ info.data.gender }}</li>
+                <!-- <li>Email: {{ info.data.email }}</li> -->
+            </ul>
+          
+        </v-card-title>
+        <v-card-actions class="center-align">
+            <v-btn color="pink lighten-2" @click="requestMember">
+                VIEW FULL PROFILE
+                </v-btn>       
+        </v-card-actions>
+      </v-card>
+      </div>
+    </v-flex>
+
+  </v-layout>
+  </v-container>
+    <v-container fluid>
+    <v-layout 
+    row
+    align-center
+    justify-center
+    ma-0
+    >
+   <v-flex v-for="i in 4" :key="i">
+  <div>
+      <v-card width="200px">
+       <v-img :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" alt="image" width="200px" height="200px">
+        
+        </v-img>
+        <v-card-title>
+            <ul>
+                <li class="headline mb-0">{{ info.data.name }}</li>
+                <li>Age: {{ info.data.age }}</li>
+                <li>Gender: {{ info.data.gender }}</li>
+                <!-- <li>Email: {{ info.data.email }}</li> -->
+            </ul>
+          
+        </v-card-title>
+        <v-card-actions class="center-align">
+            <v-btn color="pink lighten-2" @click="requestMember">
+                VIEW FULL PROFILE
+                </v-btn>       
+        </v-card-actions>
+      </v-card>
+      </div>
+    </v-flex>
+
+  </v-layout>
+  </v-container>
+   <v-container fluid>
+    <v-layout 
+    row
+    align-center
+    justify-center
+    ma-0
+    >
+   <v-flex v-for="i in 4" :key="i">
+  <div>
+      <v-card width="200px">
+       <v-img :src="`https://randomuser.me/api/portraits/women/${i + 50}.jpg`" alt="image" width="200px" height="200px">
         
         </v-img>
         <v-card-title>
