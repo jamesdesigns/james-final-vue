3298e10aea2c0e0f65c7456a466df8db70e26a35
James Hooper <jameshooperdesigns@yahoo.com>
2018-12-05 13:12:33 -0700

    Revise the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="about">
     <h1 class="animated bounceIn">How it Works</h1>
-    <p>Build a team the easy way. Find individuals you are interested in teaming up with and conquer the gaming world. Simply select the games you want to play and who you would like on your new team.</p> 
+    <p>Build a team the easy way. Find individuals you are interested in teaming up with and conquer the gaming world. Login and set up a password and enter your game experience level. Then select the games you want to play and who you would like on your new team.</p> 
     <br>
-    <p>Choose individuals who might benefit you the most. Fill out the form and click SEND REQUEST. Each hand picked player will either approve or disapprove your request. Good Luck!</p>
+    <p>Choose individuals who might benefit you the most. After filling out the form, click SEND REQUEST. Each hand picked player will either approve or disapprove your request. Good Luck!</p>
   </div>
 </template>
 <script>
