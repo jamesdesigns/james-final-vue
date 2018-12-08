81070172885678c50ccaa6ad62e48d70e59db3be
James Hooper <jameshooperdesigns@yahoo.com>
2018-12-08 14:12:44 -0700

    Fix the v-for loop on the Members page

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -8,19 +8,21 @@
     justify-center
     ma-0
     >
-    <v-flex v-for="i in info" :key="i">
+
+    <v-flex v-for="(i, index) in info" :key="index">
+    <!-- <v-flex v-for="i in info"> -->
    <!-- <v-flex v-for='(item, i) in info' :key="i"> -->
   <div>
       <v-card width="200px">
       <!-- <v-img :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" alt="image" width="200px" height="200px"> -->
         <!-- <v-img :src="`https://uinames.com/api/photos/female/${i}.jpg`" alt="image" width="200px" height="200px"> -->
-        <v-img :src="info.data.photo" alt="image" width="200px" height="200px">
+        <v-img :src="i.photo" alt="image" width="200px" height="200px">
         </v-img>
         <v-card-title>
             <ul>
-                <li class="headline mb-0">{{ info.data.name }}</li>
-                <li>Age: {{ info.data.age }}
-                <li>Gender: {{ info.data.gender }}</li>    
+                <li class="headline mb-0">{{ i.name }}</li>
+                <li>Age: {{ i.age }}
+                <li>Gender: {{ i.gender }}</li>    
                 <!-- <li class="headline mb-0">{{ item.name }}</li>
                 <li>Age: {{ item.age }}</li>
                 <li>Gender: {{ item.gender }}</li> -->
@@ -36,47 +38,14 @@
       </v-card>
       </div>
     </v-flex>
+   
 
   </v-layout>
   </v-container>
 
   
 
-  
-   <v-container fluid>
-    <v-layout 
-    row
-    align-center
-    justify-center
-    ma-0
-    >
-    <v-flex v-for="i in info" :key="i">
-   <!-- <v-flex v-for="i in 4" :key="i"> -->
-  <div>
-      <v-card width="200px">
-      <!-- <v-img :src="`https://uinames.com/api/photos/female/${i}.jpg`" alt="image" width="200px" height="200px"> -->
-        <v-img :src="info.data.photo" alt="image" width="200px" height="200px">
-       <!-- <v-img :src="`https://randomuser.me/api/portraits/women/${i}.jpg`" alt="image" width="200px" height="200px"> -->
-        </v-img>
-        <v-card-title> 
-            <ul>
-                <li class="headline mb-0">{{ info.data.name }}</li>
-                <li>Age: {{ info.data.age }}</li>
-                <li>Gender: {{ info.data.gender }}</li>
-            </ul>
-          
-        </v-card-title>
-        <v-card-actions class="center-align">
-            <v-btn color="pink lighten-2" @click="requestMember">
-                VIEW PROFILE
-                </v-btn>       
-        </v-card-actions>
-      </v-card>
-      </div>
-    </v-flex>
-
-  </v-layout>
-  </v-container>
+ 
   </div>
 
 </template>
@@ -94,25 +63,27 @@ export default {
    // 'https://uinames.com/api/?ext&?amount=50'
     // New example
     mounted() {
-        axios
-            .get('https://uinames.com/api/?ext')
-           // .get('https://randomuser.me/api/?inc=gender,name,picture')
-            .then(response => (this.info = response))
-    },
-    methods: {
-        requestMember() {
-            return axios
-            // Original UINAMES used
-             .get('https://uinames.com/api/?ext')
-            // .get('https://randomuser.me/api/?inc=gender,name,picture')
-            .then(function(response) {
-                this.info.push(response.data.results[4].name)
-            })
-            .catch(function(error) {
-                console.log(error)
-            })
-        }
-    } 
+         axios
+             .get('https://uinames.com/api/?amount=5&ext')
+    //        // .get('https://randomuser.me/api/?inc=gender,name,picture')
+             .then(response => (this.info = response.data))
+     },
+   // methods: {
+    //     requestMember() {
+    //         return axios
+    //         // Original UINAMES used
+    //          .get('https://uinames.com/api/?amount=5')
+    //         // .get('https://randomuser.me/api/?inc=gender,name,picture')
+    //         .then(function(response) {
+    //            // this.info.push(response.data.results[4].name)
+    //            console.log(response.data);
+    //            this.info = response.data;
+    //         })
+    //         .catch(function(error) {
+    //             console.log(error)
+    //         })
+    //     }
+    // } 
 }
 </script>
 <style>
